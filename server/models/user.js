0660c98999e4596ed714945faa95a8c45002835f
Aleksandr Golovatšjov <aleksandrgol@ed-aleksandrgol-mac.ekspress.ee>
2018-01-11 10:46:18 +0200

    POST /users

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,37 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('Users', {
     email: {
         type: String,
         required: true,
         trim: true,
-        minLength: 1
-    }
+        minLength: 100,
+        unique: true,
+        validate: {
+            // validator: (value) => {
+            //     return validator.isEmail(value);
+            // },
+            validator: validator.isEmail,
+            message: '{value} is not valid email'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+
 });
 
 module.exports = {User};
