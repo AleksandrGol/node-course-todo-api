c3708602423e0e2a6a9b080baf5f5ee1c62534bf
Aleksandr Golovatšjov <aleksandrgol@ed-aleksandrgol-mac.ekspress.ee>
2018-01-11 13:07:52 +0200

    add password hash

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 4
-};
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    })
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log('Decoded:', decoded);
+var hashedPassword = '$2a$10$logYO9y6bC4Ld1GxdSAJGujoBgcEEx.38UOHvym.KP7mvpoUG4AaO';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+
+
+// var data = {
+//     id: 4
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('Decoded:', decoded);
 
 // var message = 'I am user nr 1';
 // var hash = SHA256(message).toString();
